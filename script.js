6f753c1e21260292a4f0afd8798f97cf8fa3a7f4
Zaw <42999156+ImZaw@users.noreply.github.com>
2024-06-18 12:57:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function generateQuestions() {
       <div class="card" id="question${i}" style="display: ${i === 1 ? 'block' : 'none'};">
         <h3>Question ${i}</h3>
         <div class="options-container">
-          <button type="button" class="option" onclick="chooseOption('a', ${i})" data-option="a">A</button>
-          <button type="button" class="option" onclick="chooseOption('b', ${i})" data-option="b">B</button>
-          <button type="button" class="option" onclick="chooseOption('c', ${i})" data-option="c">C</button>
-          <button type="button" class="option" onclick="chooseOption('d', ${i})" data-option="d">D</button>
+          <button type="button" class="option" onclick="chooseOption('أ', ${i})" data-option="أ">أ</button>
+          <button type="button" class="option" onclick="chooseOption('ب', ${i})" data-option="ب">ب</button>
+          <button type="button" class="option" onclick="chooseOption('ج', ${i})" data-option="ج">ج</button>
+          <button type="button" class="option" onclick="chooseOption('د', ${i})" data-option="د">د</button>
         </div>
         <input type="hidden" id="q${i}" name="q${i}" value="">
       </div>
